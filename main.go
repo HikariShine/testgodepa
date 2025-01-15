379392a98c49b847bcab4cb68d0260a88a237c6e
光闪 <guangshan1992@qq.com>
2025-01-15 22:18:17 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package packagea
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func Test() {
+	logrus.Errorf("configs err: %s", "agfdfs")
+}
